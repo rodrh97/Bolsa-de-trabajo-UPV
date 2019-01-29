abdefd128ca6fb777132a51d020b82a4d425400b
Rodrigo <1530219@upv.edu.mx>
2019-01-29 13:53:12 -0600

    Cambio 42

--- resources/views/egresado/layout.blade.php
+++ resources/views/egresado/layout.blade.php
@@ -49,5 +49,38 @@
       $('[data-toggle="tooltip"]').tooltip()
     })
 </script>
+<!-- Required Jquery -->
+<script type="text/javascript" src="../bower_components/jquery/js/jquery.min.js"></script>
+<script type="text/javascript" src="../bower_components/jquery-ui/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="../bower_components/popper.js/js/popper.min.js"></script>
+<script type="text/javascript" src="../bower_components/bootstrap/js/bootstrap.min.js"></script>
+<!-- jquery slimscroll js -->
+<script type="text/javascript" src="../bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+<!-- modernizr js -->
+<script type="text/javascript" src="../bower_components/modernizr/js/modernizr.js"></script>
+<script type="text/javascript" src="../bower_components/modernizr/js/css-scrollbars.js"></script>
+
+
+<!-- i18next.min.js -->
+<script type="text/javascript" src="../bower_components/i18next/js/i18next.min.js"></script>
+<script type="text/javascript" src="../bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js"></script>
+<script type="text/javascript"
+        src="../bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js"></script>
+<script type="text/javascript" src="../bower_components/jquery-i18next/js/jquery-i18next.min.js"></script>
+<!-- Select 2 js -->
+<script type="text/javascript" src="../bower_components/select2/js/select2.full.min.js"></script>
+<!-- Multiselect js -->
+<script type="text/javascript" src="../bower_components/bootstrap-multiselect/js/bootstrap-multiselect.js">
+
+
+</script>
+<script type="text/javascript" src="../bower_components/multiselect/js/jquery.multi-select.js"></script>
+<script type="text/javascript" src="assets/js/jquery.quicksearch.js"></script>
+<!-- Custom js -->
+<script type="text/javascript" src="assets/pages/advance-elements/select2-custom.js"></script>
+<script src="assets/js/pcoded.min.js"></script>
+<script src="assets/js/demo-12.js"></script>
+<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
